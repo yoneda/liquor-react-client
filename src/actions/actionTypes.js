a7b134d91811cf56575362846f5d241773dfb979
yoneda <nsshowdream@gmail.com>
2019-12-04 22:16:10 +0900

    [add] add some action types

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,1 +1,9 @@
-export const TEST_ACTION = "TEST_ACTION";
+
+// レシピ関連
+export const RECIPES_LOADED = "RECIPES_LOADED";
+export const STARS_LOADED = "STARS_LOADED";
+export const POSTS_LOADED = "POSTS_LOADED";
+export const PICK_RECIPE = "PICK_RECIPE";
+
+// プロフィール関連
+export const PICK_RECIPE = "PICK_RECIPE";
