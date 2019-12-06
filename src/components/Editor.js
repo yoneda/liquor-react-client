dc189beae3c71648a7bad440781810ea681f60d8
yoneda <nsshowdream@gmail.com>
2019-12-06 19:34:33 +0900

    [fix] rough implement Editor UI

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,32 +1,68 @@
-import React, {use} from "react";
+import React from "react";
 import { connect } from "react-redux";
+import { useLegacyState } from "../hooks";
 
-const mapStateToProps = state => ({
-  editor: state.editor,
-});
+const mapDispatchToProps = dispatch => ({});
 
 const Editor = props => {
-  const { title, cookTime, cost, ingredients, directions } = props.editor;
+  const [state, setState] = useLegacyState({
+    title: "",
+    cookTime: undefined,
+    cost: undefined,
+    ingredients: "",
+    directions: ""
+  });
   return (
     <div>
       <h2>新規投稿</h2>
-      <div>タイトル: {title}</div>
-      <div>調理時間: {cookTime}分</div>
-      <div>費用: {cost}円</div>
       <div>
-        <span>材料: </span>
-        {ingredients.map(ingredient => (
-          <span>{ingredient}</span>
-        ))}
+        レシピ名: <br />
+        <input
+          type="text"
+          style={{ width: "300px" }}
+          value={state.title}
+          onChange={event => setState({ title: event.target.value })}
+        />
       </div>
       <div>
-        <span>手順: </span>
-        {directions.map(direction => (
-          <span>{direction}</span>
-        ))}
+        時間: <br />
+        <input
+          type="number"
+          style={{ width: "300px" }}
+          value={state.cookTime}
+          onChange={event => setState({ cookTime: event.target.value })}
+        />
+      </div>
+      <div>
+        費用: <br />
+        <input
+          type="number"
+          style={{ width: "300px" }}
+          value={state.cost}
+          onChange={event => setState({ cost: event.target.value })}
+        />
+      </div>
+
+      <div>
+        材料: <br />
+        <input
+          type="text"
+          style={{ width: "300px" }}
+          value={state.ingredients}
+          onChange={event => setState({ ingredients: event.target.value })}
+        />
+      </div>
+      <div>
+        手順: <br />
+        <input
+          type="text"
+          style={{ width: "300px" }}
+          value={state.directions}
+          onChange={event => setState({ directions: event.target.value })}
+        />
       </div>
     </div>
   );
 };
 
-export default connect(mapStateToProps)(Editor);
+export default connect(null, mapDispatchToProps)(Editor);
