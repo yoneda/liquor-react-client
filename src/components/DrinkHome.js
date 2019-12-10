6e97c7f71d1c3f4af8b92c2eb5139a65b2dbe436
yoneda <nsshowdream@gmail.com>
2019-12-10 17:58:31 +0900

    [fix] make it possible to read data on DrinkHome

--- src/components/DrinkHome.js
+++ src/components/DrinkHome.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+const GET_DRINKS = gql`
+  query {
+    allDrinks(category: COCKTAIL) {
+      id
+      name
+      url
+    }
+  }
+`;
 
 const DrinkHome = props => {
-    return (
+  const { loading, error, data } = useQuery(GET_DRINKS);
+  if (loading) return <div>loading</div>;
+  if (error) return <div>error:{error}</div>;
+  return (
+    <div>
+      {data.allDrinks.map(drink => (
         <div>
-          home
+          <div>{drink.name}</div>
+          <img src={`${drink.url}`} width="50" />
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default DrinkHome;
